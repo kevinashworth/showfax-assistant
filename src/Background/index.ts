584f792c889e067a3bd8bdb6b6b8c4c21412b5af
Kevin Ashworth <kevinashworth@yahoo.com>
2020-09-03 12:04:12 -0700

    [wip]

--- src/Background/index.ts
+++ src/Background/index.ts
@@ -1,6 +1,8 @@
 import 'emoji-log';
 import { browser, Tabs } from 'webextension-polyfill-ts';
 
+let saTabId = null;
+
 const onInstalledHandler = (details): void => {
   console.emoji('🦄', 'Showfax Assistant installed');
   if (details.reason === "install") {
@@ -17,8 +19,9 @@ browser.runtime.onInstalled.addListener(onInstalledHandler);
 const onTabUpdateHandler = (tabId: number, changeInfo: Tabs.OnUpdatedChangeInfoType, tab: Tabs.Tab) => {
   console.groupCollapsed('onTabUpdateHandler:');
   console.log('tab id:', tabId);
+  saTabId = tabId;
   console.log('changeInfo:', changeInfo);
-  console.dir('tab:', tab);
+  console.log('tab:', tab);
   console.groupEnd()
 }
 browser.tabs.onUpdated.addListener(onTabUpdateHandler);
@@ -27,3 +30,30 @@ const onMessageHandler = (message: any): void => {
   console.log('Showfax Assistant message in Background/index:', message);
 }
 browser.runtime.onMessage.addListener(onMessageHandler);
+
+browser.tabs.sendMessage(saTabId, {
+  "title": "run",
+  "message": "do it"
+});
+
+
+function onError(error) {
+  console.error(`Error: ${error}`);
+}
+
+function sendMessageToTabs(tabs) {
+  for (let tab of tabs) {
+    browser.tabs.sendMessage(
+      tab.id,
+      { greeting: "Hi from Showfax Assistant background script" }
+    ).then(response => {
+      console.log("Message from the SA content script:");
+      console.log(response.response);
+    }).catch(onError);
+  }
+}
+
+browser.tabs.query({
+  currentWindow: true,
+  active: true
+}).then(sendMessageToTabs).catch(onError);
--- src/ContentScript/index.ts
+++ src/ContentScript/index.ts
@@ -50,9 +50,9 @@ import { browser } from 'webextension-polyfill-ts';
 // if (document.readyState != 'loading') console.log('DOM should be ready.')
 // console.log('Hello from runContentScript');
 
-const observerConfigSm = {
-  attributes: true
-};
+// const observerConfigSm = {
+//   attributes: true
+// };
 
 // const observerConfigLg = {
 //   attributes: true,
@@ -60,73 +60,125 @@ const observerConfigSm = {
 //   subtree: true
 // };
 
-const sleep = (ms: number) => {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+// const sleep = (ms: number) => {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
 
-async function runContentScript () {
-  console.log('Hello...');
+// async function runContentScript () {
+//   console.log('Hello...');
 
-  let state = {
-    resultsContainer: false,
-    resultsLoading: false,
-    resultsRegion: false,
-    resultsQuick: false
-  }
+//   let state = {
+//     resultsContainer: false,
+//     resultsLoading: false,
+//     resultsRegion: false,
+//     resultsQuick: false
+//   }
 
-  var regionSearchButton = <HTMLButtonElement>document.querySelector('button[name="searchMethodButtons_region"]');
-  // var regionSearchButtonObserver = new MutationObserver(function (mutations) {
-  //   mutations.forEach(function (mutation) {
-  //     const el = mutation.target as HTMLElement;
-  //     console.log('regionSearchButtonObserver:', mutation, el);
-  //   });
-  // });
-  // regionSearchButtonObserver.observe(regionSearchButton, observerConfigLg);
+//   var regionSearchButton = <HTMLButtonElement>document.querySelector('button[name="searchMethodButtons_region"]');
+//   // var regionSearchButtonObserver = new MutationObserver(function (mutations) {
+//   //   mutations.forEach(function (mutation) {
+//   //     const el = mutation.target as HTMLElement;
+//   //     console.log('regionSearchButtonObserver:', mutation, el);
+//   //   });
+//   // });
+//   // regionSearchButtonObserver.observe(regionSearchButton, observerConfigLg);
 
-  await sleep(500);
-  console.log('...from runContentScript');
+//   await sleep(500);
+//   console.log('...from runContentScript');
 
-  var targetObserver = new MutationObserver(function (mutations) {
-    mutations.forEach(function (mutation) {
-      const el = mutation.target as HTMLElement;
-      if (el.classList.contains('results-container-container') && el.classList.contains('hidden')) {
-        state = { ...state, resultsContainer: false }
-      } else if (el.classList.contains('results-container-container') && !el.classList.contains('hidden')) {
-        state = { ...state, resultsContainer: true }
-      } else if (el.classList.contains('results-none') && el.classList.contains('hidden')) {
-        state = { ...state, resultsLoading: false }
-      } else if (el.classList.contains('results-none') && !el.classList.contains('hidden')) {
-        state = { ...state, resultsLoading: true }
-      } else if (el.classList.contains('results-region') && el.classList.contains('hidden')) {
-        state = { ...state, resultsRegion: false }
-      } else if (el.classList.contains('results-region') && !el.classList.contains('hidden')) {
-        state = { ...state, resultsRegion: true }
-      } else if (el.classList.contains('results-quick') && el.classList.contains('hidden')) {
-        state = { ...state, resultsQuick: false }
-      } else if (el.classList.contains('results-quick') && !el.classList.contains('hidden')) {
-        state = { ...state, resultsQuick: true }
-      }
-    });
-    console.table(state);
-  });
+//   var targetObserver = new MutationObserver(function (mutations) {
+//     mutations.forEach(function (mutation) {
+//       const el = mutation.target as HTMLElement;
+//       if (el.classList.contains('results-container-container') && el.classList.contains('hidden')) {
+//         state = { ...state, resultsContainer: false }
+//       } else if (el.classList.contains('results-container-container') && !el.classList.contains('hidden')) {
+//         state = { ...state, resultsContainer: true }
+//       } else if (el.classList.contains('results-none') && el.classList.contains('hidden')) {
+//         state = { ...state, resultsLoading: false }
+//       } else if (el.classList.contains('results-none') && !el.classList.contains('hidden')) {
+//         state = { ...state, resultsLoading: true }
+//       } else if (el.classList.contains('results-region') && el.classList.contains('hidden')) {
+//         state = { ...state, resultsRegion: false }
+//       } else if (el.classList.contains('results-region') && !el.classList.contains('hidden')) {
+//         state = { ...state, resultsRegion: true }
+//       } else if (el.classList.contains('results-quick') && el.classList.contains('hidden')) {
+//         state = { ...state, resultsQuick: false }
+//       } else if (el.classList.contains('results-quick') && !el.classList.contains('hidden')) {
+//         state = { ...state, resultsQuick: true }
+//       }
+//     });
+//     console.table(state);
+//   });
 
-  var targetResultsContainer = <HTMLElement>document.getElementById('results_container');
-  var targetResultsLoading = <HTMLElement>document.getElementById('results_loading');
-  var targetResultsQuick = <HTMLElement>document.getElementById('results_quick');
-  var targetResultsRegion = <HTMLElement>document.getElementById('results_region');
-  console.log(targetResultsContainer);
-  console.log(targetResultsLoading);
-  console.log(targetResultsQuick);
-  console.log(targetResultsRegion);
-  targetObserver.observe(targetResultsContainer, observerConfigSm);
-  targetObserver.observe(targetResultsLoading, observerConfigSm);
-  targetObserver.observe(targetResultsQuick, observerConfigSm);
-  targetObserver.observe(targetResultsRegion, observerConfigSm);
+//   var targetResultsContainer = <HTMLElement>document.getElementById('results_container');
+//   var targetResultsLoading = <HTMLElement>document.getElementById('results_loading');
+//   var targetResultsQuick = <HTMLElement>document.getElementById('results_quick');
+//   var targetResultsRegion = <HTMLElement>document.getElementById('results_region');
+//   console.log(targetResultsContainer);
+//   console.log(targetResultsLoading);
+//   console.log(targetResultsQuick);
+//   console.log(targetResultsRegion);
+//   targetObserver.observe(targetResultsContainer, observerConfigSm);
+//   targetObserver.observe(targetResultsLoading, observerConfigSm);
+//   targetObserver.observe(targetResultsQuick, observerConfigSm);
+//   targetObserver.observe(targetResultsRegion, observerConfigSm);
 
-  if (regionSearchButton && !(state.resultsContainer)) {
-    console.log('About to click regionSearchButton.');
+//   if (regionSearchButton && !(state.resultsContainer)) {
+//     console.log('About to click regionSearchButton.');
+//     regionSearchButton.click();
+
+//     const regionSelector = <HTMLSelectElement>document.querySelector('select[name="regionValue"]');
+//     regionSelector.options[1].selected = true; // Los Angeles
+
+//     const categorySelector = <HTMLSelectElement>document.querySelector('select[name="categoryValue"]');
+//     categorySelector.options[1].selected = true; // Episodics
+
+//     const formSearchButton = <HTMLInputElement>document.querySelector('input[name="regionButton"]')
+//     formSearchButton.click();
+
+//     // const sortByDropdown = <HTMLSelectElement>document.querySelector('select[name="sort-search_region"]');
+//     // console.log(sortByDropdown);
+//     // const sortByDropdownObserver = new MutationObserver((mutations) => {
+//     //   mutations.forEach((mutation) => {
+//     //     console.table(mutation);
+//     //     // const el = mutation.target as HTMLElement;
+//     //   })
+//     // })
+//     // sortByDropdownObserver.observe(sortByDropdown, observerConfigLg);
+//     // const sortByClickable = <HTMLElement>document.querySelector('#results_region div.div-selector');
+//     // console.log(sortByClickable);
+//     // sortByClickable.click();
+//     const sortByUL = <HTMLElement>document.querySelector('#results_region ul');
+//     console.log(sortByUL);
+//     sortByUL.style.display = 'block';
+//     // sortByUL.click();
+//     const newestLI = <HTMLUListElement>document.querySelector('li.option-selector[rel="Newest"]')
+//     console.log(newestLI);
+//     newestLI.click();
+//     // const sortByULObserver = new MutationObserver((mutations) => {
+//     //   mutations.forEach((mutation) => {
+//     //     console.table(mutation);
+//     //     // const el = mutation.target as HTMLElement;
+//     //   })
+//     // })
+//     // sortByULObserver.observe(sortByUL, observerConfigLg);
+
+//     console.table(state);
+//   }
+
+//   // sortByDropdown.options[0].selected = true; // Newest
+
+//   console.log('Goodbye from runContentScript');
+// }
+
+const run = async () => {
+  var el = document.querySelector('input[name="regionButton"]')
+  var style = window.getComputedStyle(el)
+  var searchVisible = (style.visibility === 'visible')
+  if (!searchVisible) {
+    var regionSearchButton = <HTMLButtonElement>document.querySelector('button[name="searchMethodButtons_region"]');
     regionSearchButton.click();
-
+  } else {
     const regionSelector = <HTMLSelectElement>document.querySelector('select[name="regionValue"]');
     regionSelector.options[1].selected = true; // Los Angeles
 
@@ -135,43 +187,10 @@ async function runContentScript () {
 
     const formSearchButton = <HTMLInputElement>document.querySelector('input[name="regionButton"]')
     formSearchButton.click();
-
-    // const sortByDropdown = <HTMLSelectElement>document.querySelector('select[name="sort-search_region"]');
-    // console.log(sortByDropdown);
-    // const sortByDropdownObserver = new MutationObserver((mutations) => {
-    //   mutations.forEach((mutation) => {
-    //     console.table(mutation);
-    //     // const el = mutation.target as HTMLElement;
-    //   })
-    // })
-    // sortByDropdownObserver.observe(sortByDropdown, observerConfigLg);
-    // const sortByClickable = <HTMLElement>document.querySelector('#results_region div.div-selector');
-    // console.log(sortByClickable);
-    // sortByClickable.click();
-    const sortByUL = <HTMLElement>document.querySelector('#results_region ul');
-    console.log(sortByUL);
-    sortByUL.style.display = 'block';
-    // sortByUL.click();
-    const newestLI = <HTMLUListElement>document.querySelector('li.option-selector[rel="Newest"]')
-    console.log(newestLI);
-    newestLI.click();
-    // const sortByULObserver = new MutationObserver((mutations) => {
-    //   mutations.forEach((mutation) => {
-    //     console.table(mutation);
-    //     // const el = mutation.target as HTMLElement;
-    //   })
-    // })
-    // sortByULObserver.observe(sortByUL, observerConfigLg);
-
-    console.table(state);
   }
-
-  // sortByDropdown.options[0].selected = true; // Newest
-
-  console.log('Goodbye from runContentScript');
 }
 
-console.emoji('🦄', 'Showfax Assistant loaded');
+console.emoji('🦄', 'Showfax Assistant ContentScript/index loaded');
 
 const sendToBackgroundScript = (e: Event) => {
   console.log('sendToBackgroundScript in ContentScript/index:');
@@ -182,5 +201,19 @@ const sendToBackgroundScript = (e: Event) => {
   });
 }
 window.addEventListener("click", sendToBackgroundScript);
+
+// const onMessageHandler = (message: any): void => {
+//   console.log('Showfax Assistant received message in ContentScript/index:', message);
+//   if (message.title === 'run') {
+//     run();
+//   }
+// }
+// browser.runtime.onMessage.addListener(onMessageHandler);
+
+browser.runtime.onMessage.addListener(request => {
+  console.log("Message from the SA background script:");
+  console.log(request.greeting);
+  return Promise.resolve({ response: "Hi from Showfax Assistant content script" });
+});
 
 export {};
